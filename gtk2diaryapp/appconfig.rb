4367246263f1d87628700cfef78fc911e28ac3cf
Carlos Hernandez <carlosjhr64@gmail.com>
2009-08-06 17:58:58 -0700

    realized this needs to be minor version upgrade 1.1.0, not just a build update 1.0.2

--- gtk2diaryapp/appconfig.rb
+++ gtk2diaryapp/appconfig.rb
@@ -4,4 +4,11 @@ module Configuration
   LABELS_CLOUD_WIDTH = 32
   DEFAULT_LABEL = 'Today'
   PANE_POSITION	= 310
+  ACTIVE_TIME_FRAME = 0
+
+# open/close are so fast, dock seems wasteful.
+# MENU[:dock] = '_Dock'	# Dock only hides GUI
+  MENU[:close] = '_Close' #  Close destroys GUI, but keeps daemon running. Goes to tray.
+
+  GUI[:window_size] = [750, 500]
 end
